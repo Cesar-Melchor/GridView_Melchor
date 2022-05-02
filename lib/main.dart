3ec9a4456cd15319b3889ada110b34eabb6cd496
FlutLab.io <>
2022-05-02 19:12:22 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:melchor/gridviewarticulos.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,39 +17,3 @@ class MyApp extends StatelessWidget {
         home: MyHomePage());
   }
 } //Widget Sin Estado
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-} //Widget Con Estado
-
-class _MyHomePageState extends State<MyHomePage> {
-  List<String> images = [
-    "assets/images/Celulares.jpg",
-    "assets/images/Airpods.png",
-    "assets/images/Procesador.png",
-    "assets/images/Mac.jpg",
-    "assets/images/watch.jpg",
-    "assets/images/Laptop.jpg",
-    "assets/images/Logo.png",
-    "assets/images/Green.jpg",
-    "assets/images/AirTag.jpg",
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter GridView"),
-        ),
-        body: GridView.builder(
-          itemCount: images.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Image.asset(images[index], fit: BoxFit.cover);
-          },
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3, mainAxisSpacing: 10, crossAxisSpacing: 10),
-          padding: EdgeInsets.all(10),
-          shrinkWrap: true,
-        ));
-  }
-}
